9c27d6d8375fe24458f1a97495d195b851226608
Travis Brady <travis@.(none)>
2009-06-02 01:22:08 -0400

    fix the mutator type issues and get everything working, add some printfs

--- tsp.ml
+++ tsp.ml
@@ -1,17 +1,15 @@
 open Printf
 module R=Random
 
-let mut_prob = 0.5
+let mut_prob = 0.8
+let cross_prob = 0.60
 let opt_file = "data/eil51.opt.tour"
 let file = "data/eil51.tsp"
 let num = Str.regexp "^[0-9]"
 let space = Str.regexp " "
-let sum = List.fold_left (+) 0
-
+let list_min = List.fold_left min 1000000
 let split_coords x = Str.split space x
-
 let nint x = truncate(x +. 0.5);;
-
 let dist (_, x1, y1) (_, x2, y2) = 
     nint(sqrt(((x1 -. x2)**2.0 +. (y1 -. y2)**2.0)))
 
@@ -19,16 +17,14 @@ let dist (_, x1, y1) (_, x2, y2) =
 let make_city = function
       [num; x; y] -> Some (int_of_string num, float_of_string x, float_of_string y)
     | _ -> None
-;;
 
 let de_option l = List.rev (List.fold_left (fun acc x ->
     match x with
         None -> acc
-        | Some thing -> thing::acc) [] l);;
+        | Some thing -> thing::acc) [] l)
 
 let create_dist_map cities = List.map (fun x ->
     List.map (fun y -> dist x y) cities) cities
-;;
 
 let make_dist_array cities = Array.of_list 
     (List.map Array.of_list (create_dist_map cities))
@@ -81,29 +77,20 @@ let init_proc =
     let cities = de_option(List.map make_city hey) in
     make_dist_array cities
 
-let rec two_opt dist_machine tour = 
-    let tour_len = (List.length tour + 1) in
-    let ll = [R.int tour_len; R.int tour_len] in
-    let [si; ei] = List.sort compare ll in
-    let _start = Util.take si tour in
-    let mid = List.rev (Util.take (ei - si) (Util.drop si tour)) in
-    let _end = Util.drop ei tour in
-    let _done = List.concat [_start; mid; _end] in
-    match (dist_machine _done ) < (dist_machine tour) with
-        true -> two_opt dist_machine _done
-        | false -> tour
-
 let make_tour ncities = Util.shuffle(Util.range 2 ncities)
-
 let make_many_tours ncities ntours = List.map (fun x -> 
     make_tour ncities) (Util.range 1 ntours)
 
-let get_next_city tour city = 
-    let ind = List.find (fun x -> x==city) tour in
-    List.nth tour ind
-
-let list_max = List.fold_left max 0
-let list_min = List.fold_left min 1000000
+let get_next_city tour city =
+    let rec loop t = match t with
+        | [] -> raise Not_found
+        | [last] -> 1
+        | (curr::next::rest) when curr==city -> next
+        | (curr::rest) -> loop rest
+    in
+    match city with
+    | 1 -> List.hd tour
+    | x -> loop tour
 
 let random_of xs n = List.nth xs (R.int n)
 
@@ -117,8 +104,8 @@ let tournament dist_machine n tours =
 let choose_one dist_array city mom dad not_picked =
     let mom_next = get_next_city mom city in
     let dad_next = get_next_city dad city in
-    let mom_dist = dist_array.(city).(mom_next) in
-    let dad_dist = dist_array.(city).(dad_next) in
+    let mom_dist = dist_array.(city - 1).(mom_next - 1) in
+    let dad_dist = dist_array.(city - 1).(dad_next - 1) in
     match mom_dist <= dad_dist && List.mem mom_next not_picked with
           true -> mom_next
         | false -> match dad_dist <= mom_dist 
@@ -126,9 +113,28 @@ let choose_one dist_array city mom dad not_picked =
               true -> dad_next
             | false -> List.hd not_picked
 
+let mutate dist_machine tour = 
+    let rec two_opt tour = 
+        let tour_len = (List.length tour + 1) in
+        let ll = [R.int tour_len; R.int tour_len] in
+        let [si; ei] = List.sort compare ll in
+        let _start = Util.take si tour in
+        let mid = List.rev (Util.take (ei - si) (Util.drop si tour)) in
+        let _end = Util.drop ei tour in
+        let _done = List.concat [_start; mid; _end] in
+        match (dist_machine _done ) < (dist_machine tour) with
+              true -> two_opt _done
+            | false -> tour
+    in
+    let mut_roll = R.float 1.0 in
+    match mut_roll < mut_prob with
+          true -> two_opt tour
+        | false -> tour
+
 let rec greedy_cross chooser mom dad not_picked acc =
     match not_picked with
         | [] -> acc
+        | [last] -> last::acc
         | _ ->
         let city = match acc with
             | [] -> 1
@@ -137,29 +143,56 @@ let rec greedy_cross chooser mom dad not_picked acc =
         let the_chosen = chooser city mom dad not_picked in
             greedy_cross chooser mom dad (Util.delete the_chosen not_picked) (the_chosen::acc)
 
-let mutate mutator tour = 
-    let mut_roll = R.float 1.0 in
-    match mut_roll < mut_prob with
-          true -> mutator tour
+let maybe_cross chooser tourn_machine tour pop =
+    let cross_roll = R.float 1.0 in
+    match cross_roll < cross_prob with
         | false -> tour
-
-let _ = 
-
+        | true -> let dad = tourn_machine pop in
+            greedy_cross chooser tour dad tour []
+let test_maybe_cross =
     let dm = init_proc in
     let dist_machine = tour_dist dm in
-    let opt_tour = get_opt_tour in
-    let otd = tour_dist dm opt_tour in 
-    printf "%i\n" otd;
-    let tour = make_tour 51 in
     let tours = make_many_tours 51 100 in
-    printf "%i\n" (List.length tours);
-    printf "%i\n" (get_next_city tour 7);
-    let tour_dists = List.map (tour_dist dm) tours in
-    printf "%i\n" (List.length tour_dists);
-    (* let minny = list_min tour_dists in *)
-    let winner = tournament dist_machine 3 tours in
     let chooser = choose_one dm in
     let tourn_machine = tournament dist_machine 3 in
-    printf "%s\n" "hi";
+    let crosser = maybe_cross chooser tourn_machine in
+    let t = List.hd tours in
+    let winner = tourn_machine tours in
+    mutate dist_machine winner
 
+let gen_generation crosser tourn_machine mutator pop pop_size =
+    let rec gen_generation' pop_size acc = 
+        match pop_size with
+        | 0 -> acc
+        | _ -> 
+            let mom = tourn_machine pop in
+            let this_tour = crosser mom pop in
+            let mutant = mutator this_tour in
+            gen_generation' (pop_size - 1) (mutant::acc)
+    in
+    gen_generation' pop_size []
+
+let rec keep_on dist_machine genmach pop generations =
+    match generations with
+        | 0 -> pop
+        | x -> let ng = genmach pop 500
+        in
+            let dists = List.map dist_machine ng in
+            let mini = list_min dists in
+            if x mod 50 == 0 then printf "%d\n%!" mini else ();
+            keep_on dist_machine genmach ng (x - 1)
+    
+let _ = 
+    printf "%s\n!" "init_proc";
+    flush stdout;;
+    let dm = init_proc in
+    let dist_machine = tour_dist dm in
+    let tours = make_many_tours 51 5000 in
+    let chooser = choose_one dm in
+    let tourn_machine = tournament dist_machine 3 in
+    let crosser = maybe_cross chooser tourn_machine in
+    let gengen = gen_generation crosser tourn_machine (mutate dist_machine) in
+    printf "%s\n!" "Go!";
+    let b = keep_on dist_machine gengen tours 3000 in
+    printf "%s\n!" "hi";
 
